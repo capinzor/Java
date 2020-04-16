4b48e9aa2c27a644dac23b5709782a97cae4914f
capinzor <capinzonr@unal.edu.co>
2020-04-16 00:07:41 -0500

    First commit

---
+++ POO/src/poo/Coche.java
@@ -0,0 +1,17 @@
+package poo;
+
+public class Coche {
+	int ruedas;
+	int largo;
+	int ancho;
+	int motor;
+	int peso;
+	
+	public Coche() {
+		ruedas=4;
+		largo=400;
+		ancho=250;
+		motor=1000;
+		peso=500; 
+	}
+}
---
+++ POO/src/poo/Uso_Coche.java
@@ -0,0 +1,13 @@
+package poo;
+
+public class Uso_Coche {
+
+	public static void main(String[] args) {
+		
+		Coche Renault = new Coche(); //instanciar la clase coche
+		
+		System.out.println("Este coche tiene " + Renault.ruedas + " ruedas");
+	
+	}
+
+}
